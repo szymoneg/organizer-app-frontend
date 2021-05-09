4a284f0fe1b36b28a71f1031f68a3d805876020d
kaburen <andrzejosika06@gmail.com>
2021-05-09 02:23:39 +0200

    +edit option, +add option

--- components/Note.js
+++ components/Note.js
@@ -1,34 +1,44 @@
 import React, { useState } from "react";
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import NoteEditModal from "./NoteEditModal";
 
 const Note = (props) => {
   const [expanded, setExpanded] = useState(false);
-  const { noteTitle, noteDescription } = props;
-  const [titleSize, setTitleSize] = useState(1)
+  const { noteTitle, noteDescription, fnEdit, idNote } = props;
+  const [titleSize, setTitleSize] = useState(1);
+  const [modalEditVisible, setModalEditVisible] = useState(false);
 
-  const expandNote = () =>{
-    setExpanded( prevExpanded => !prevExpanded);
+  const expandNote = () => {
+    setExpanded(prevExpanded => !prevExpanded);
     setTitleSize(!expanded ? 0 : 1);
-  }
+  };
+  const editNote = () => {
+    setModalEditVisible(true);
+  };
+
+  const closeEdit = () => {
+    setModalEditVisible(false);
+  };
 
   return (
     <View style={styles.noteContainer}>
       <View style={styles.titleContainer}>
-        <TouchableOpacity style={styles.editButton}>
+        <TouchableOpacity style={styles.editButton} onPress={() => editNote()}>
           <Icon name="pencil" size={30} color="#000" />
         </TouchableOpacity>
         <TouchableOpacity style={styles.expandButton} onPress={() => expandNote()}>
           <View style={styles.titleView}>
             <Text style={styles.titleText} numberOfLines={titleSize}>{noteTitle}</Text>
           </View>
-
           <View><Icon name="chevron-down" size={30} color="#000" /></View>
         </TouchableOpacity>
       </View>
       {expanded && <View style={styles.descriptionContainer}>
         <Text style={styles.descriptionText}>{noteDescription}</Text>
       </View>}
+      <NoteEditModal visible={modalEditVisible} title={noteTitle} desc={noteDescription} fnEdit={fnEdit}
+                     fnCancel={closeEdit} idNote={idNote} />
     </View>
   );
 };
@@ -37,8 +47,9 @@ const styles = StyleSheet.create({
   noteContainer: {
     flex: 1,
     borderWidth: 1,
-    marginTop: 24,
+    marginTop: 12,
     marginHorizontal: 24,
+    marginBottom: 12,
   },
   titleContainer: {
     flex: 0,
--- components/NotesScreen.js
+++ components/NotesScreen.js
@@ -1,19 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, SafeAreaView, FlatList } from "react-native";
-import Note from "./Note"
+import { StyleSheet, View, SafeAreaView, FlatList, TouchableOpacity } from "react-native";
+import Note from "./Note";
+import NoteAddModal from "./NoteAddModal";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const NotesScreen = () => {
   const [notesList, setNotesList] = useState([]);
+  const [modalAddVisible, setModalAddVisible] = useState(false);
+  const [refresh, setRefresh] = useState(false)
+
+
   const renderItem = ({ item }) => (
-
-    <Note noteTitle={item.title} noteDescription={item.body} />);
-
+    <Note noteTitle={item.title} noteDescription={item.body} idNote={item.id} fnEdit={editNote} />);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
-      .then(json =>  setNotesList(json))
+      .then(json => {
+        json.length = 10;
+        return json;
+      })
+      .then(json => setNotesList(json));
   }, []);
+
+  const cancelNote = () => {
+    setModalAddVisible(false);
+  };
+
+  const addNote = (note) => {
+    if (notesList.length > 0) {
+      setNotesList(oldNotesList => [...oldNotesList, note]);
+    } else {
+      setNotesList([note]);
+    }
+    setModalAddVisible(false);
+  };
+
+  const editNote = (note) => {
+    if (notesList.length > 1) {
+      let editedList = notesList;
+      editedList.splice(note.id - 1, 1, note);
+      setNotesList(editedList);
+    } else {
+      setNotesList([note]);
+    }
+    setRefresh(!refresh)
+  };
 
   return (
     <View style={styles.mainContainer}>
@@ -22,8 +54,13 @@ const NotesScreen = () => {
           data={notesList}
           renderItem={renderItem}
           keyExtractor={item => item.id}
+          extraData={refresh}
         />
+        <TouchableOpacity style={styles.buttonAdd} onPress={() => setModalAddVisible(true)}>
+          <Icon name="plus" size={30} color="#000" />
+        </TouchableOpacity>
       </SafeAreaView>
+      <NoteAddModal visible={modalAddVisible} fnCancel={cancelNote} fnAdd={addNote} length={notesList.length} />
     </View>
   );
 };
@@ -34,6 +71,19 @@ const styles = StyleSheet.create({
   },
   notesContainer: {
     flex: 1,
+  },
+  buttonAdd: {
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 56,
+    height: 56,
+    borderRadius: 50,
+    backgroundColor: "white",
+    alignSelf: "flex-end",
+    bottom: 16,
+    right: 16,
+    position: "absolute",
   },
 });
 
